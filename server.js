38e393aef04e5cf741f65eb0b6913a4d7a8c22e8
nicole <nikkihmltn1@yahoo.com>
2021-08-07 17:31:17 -0700

    set up divs, some styling to main div and background

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+
+const PORT = process.env.PORT || 4000
+
+app.use(express.static(__dirname + '/public'));
+
+app.use("/", (req, res) => {
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+app.listen(PORT, () => {
+    console.log("Up and running!")
+})
